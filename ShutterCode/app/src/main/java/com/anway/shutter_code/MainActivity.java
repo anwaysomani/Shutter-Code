2dcbb275058a40bfd722f7e44d14d738ea6b0939
Anway <anwaysomani@gmail.com>
2019-09-11 22:14:51 +0530

    Added qrCode functioning

--- ShutterCode/app/src/main/java/com/anway/shutter_code/MainActivity.java
+++ ShutterCode/app/src/main/java/com/anway/shutter_code/MainActivity.java
@@ -2,13 +2,69 @@ package com.anway.shutter_code;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView text;
+    private Button scan;
+    private IntentIntegrator qrscan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scan = (Button) findViewById(R.id.runScan);
+        text = (TextView) findViewById(R.id.finalText);
+
+        qrscan = new IntentIntegrator(this);
+        scan.setOnClickListener(this);
+    }
+
+    //Getting the scan results
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            //if qrcode has nothing in it
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show();
+            } else {
+                //if qr contains data
+                try {
+                    //converting the data to json
+                    JSONObject obj = new JSONObject(result.getContents());
+                    //setting values to textviews
+                    text.setText(obj.getString("name"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    //if control comes here
+                    //that means the encoded format not matches
+                    //in this case you can display whatever data is available on the qrcode
+                    //to a toast
+//                    Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+                    text.setText(result.getContents());
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+    @Override
+    public void onClick(View view) {
+        //initiating the qr code scan
+        qrscan.initiateScan();
     }
 }
